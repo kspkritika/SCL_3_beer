b264e3f81626a48a367361ffb1037ee5ac45d1e3
andreaortuno <andreaortuno@users.noreply.github.com>
2018-02-07 16:09:55 -0500

    wrote code.py

---
+++ code.py
@@ -0,0 +1,5 @@
+def write_beer():
+    for i in range(99, 0, -1):
+        print "%d Bottles of Beer on the wall, take one down, pass it around.\n \n"  % i
+    return "i'm done"
+print write_beer()
